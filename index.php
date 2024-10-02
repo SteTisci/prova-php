d0f4b71d9c12502e66f61a28fd7a851cb5d20bb3
SteTisci <ste.tisci@gmail.com>
2024-10-02 20:59:36 +0200

    regex per controllo formato input

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php session_start() ?>
 <html lang="en">
 
 <head>
@@ -22,7 +23,7 @@
     return $data;
   }
 
-  // Form validation, check which form is submitted and show the corresponding error messages if present.
+  // Form validation, check which form is submitted, if the input is not empty and the correct format.
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (isset($_POST['form_type']) && $_POST['form_type'] == 'register') {
@@ -31,6 +32,10 @@
         $nameErr = "Name is required";
       } else {
         $name = test_input($_POST["name"]);
+
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+          $nameErr = "Only letters and white space allowed";
+        }
       }
 
       if (empty($_POST["password"])) {
@@ -43,34 +48,59 @@
         $emailErr = "Email is required";
       } else {
         $email = test_input($_POST["email"]);
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $emailErr = "Invalid email format";
+        }
       }
 
       if (empty($_POST["phone"])) {
         $phoneErr = "Phone number is required";
       } else {
         $phone = test_input($_POST["phone"]);
+
+        if (!preg_match("/^\+?[0-9]{6,15}$/", $phone)) {
+          $phoneErr = "Only numbers allowed";
+        }
+      }
+
+      // if all input fields are correct the server redirects to the welcome page.
+      if (empty($nameErr) && empty($passwordErr) && empty($emailErr) && empty($phoneErr)) {
+        $_SESSION["name"] = $name;
+        $_SESSION["email"] = $email;
+        $_SESSION["phone"] = $phone;
+        $_SESSION["password"] = $password;
+
+        header("Location: welcome.php");
+        exit();
       }
 
     } elseif (isset($_POST['form_type']) && $_POST['form_type'] == 'login') {
 
-      if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
+      if (empty($_POST["name"])) {
+        $emailErr = "Name is required";
       } else {
-        $email = test_input($_POST["email"]);
+        $email = test_input($_POST["name"]);
       }
 
       if (empty($_POST["password"])) {
         $passwordErr = "Password is required";
       } else {
-        $password = test_input($_POST["password"]);
+        // If i use post for the password, chrome will show a security warning
+        $password = test_input($password);
+      }
+
+      // Redirect to the homepage after the login
+      if (empty($emailErr) && empty($passwordErr)) {
+        $_SESSION["email"] = $email;
+        $_SESSION["password"] = $password;
+
+        header("Location: homepage.php");
+        exit();
       }
 
     }
-    // if all input fields are correct the server redirects to the welcome page.
-    if (empty($nameErr) && empty($passwordErr) && empty($emailErr) && empty($phoneErr)) {
-      header("Location: welcome.php");
-      exit();
-    }
+
   }
   ?>
 </head>
@@ -95,7 +125,8 @@
         class="register-form <?php echo isset($_POST['form_type']) && $_POST['form_type'] == 'register' ? 'active' : ''; ?>">
         <h1>Create an account</h1>
         <div class="form-container">
-          <form action="index.php" method="post">
+          <form action="index.php" method="post" novalidate>
+            <!-- hidden input to check which form is submitted -->
             <input type="hidden" name="form_type" value="register" />
             <div class="info">
               <span>Name</span>
@@ -125,13 +156,13 @@
         class="login-form <?php echo isset($_POST['form_type']) && $_POST['form_type'] == 'login' ? 'active' : ''; ?>">
         <div class="form-container">
           <h1>Login</h1>
-          <form action="index.php" method="post">
+          <form action="index.php" method="post" novalidate>
             <input type="hidden" name="form_type" value="login" />
             <div class="info">
-              <span>Email</span>
-              <span class="error">* <?php echo $emailErr ?></span>
+              <span>Name</span>
+              <span class="error">* <?php echo $nameErr ?></span>
             </div>
-            <input type="email" name="email" placeholder="Email Address..." autocomplete="off" />
+            <input type="text" name="name" placeholder="Full name..." autocomplete="off" />
             <div class="info">
               <span>Password</span>
               <span class="error">* <?php echo $passwordErr ?></span>
