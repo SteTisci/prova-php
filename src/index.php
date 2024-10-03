74892b4e855a711aa2b4690dc81b988edb596820
SteTisci <ste.tisci@gmail.com>
2024-10-03 18:29:31 +0200

    file php organizzati nella cartella src

--- src/index.php
+++ src/index.php
@@ -5,8 +5,8 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="style.css" />
-  <script src="index.js" defer></script>
+  <link rel="stylesheet" href="../style.css" />
+  <script src="../index.js" defer></script>
   <title>Form</title>
 
   <?php include 'script.php' ?>
